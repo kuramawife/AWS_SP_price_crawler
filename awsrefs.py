3d2ee661652c3e285ebca1c4e02470adf05aa6af
kuramawife <62385006+kuramawife@users.noreply.github.com>
2023-05-02 15:19:51 +0800

    add awsrefs

---
+++ awsrefs.py
@@ -0,0 +1,60 @@
+# {Output : SavingPlanAPIRef}
+
+regions = {
+"eu-west-1":"EU (Ireland)",
+"eu-west-2":"EU (London)",
+"us-east-1":"US East (N. Virginia)",
+"us-east-2":"US East (Ohio)",
+"us-west-1":"US West (N. California)",
+"us-west-2":"US West (Oregon)",
+"ca-central-1":"Canada (Central)",
+"eu-north-1":"EU (Stockholm)",
+"eu-west-3":"EU (Paris)",
+"eu-west-2":"EU (London)",
+"eu-west-1":"EU (Ireland)",
+"eu-central-1":"EU (Frankfurt)",
+"ap-south-1":"Asia Pacific (Mumbai)",
+"ap-northeast-1":"Asia Pacific (Tokyo)",
+"ap-northeast-2":"Asia Pacific (Seoul)",
+"ap-northeast-3":"Asia Pacific (Osaka)",
+"ap-southeast-1":"Asia Pacific (Singapore)",
+"ap-southeast-2":"Asia Pacific (Sydney)",
+"sa-east-1":"South America (São Paulo)",
+"cn-north-1":"China (Beijing)",
+"cn-northwest-1":"China (Ningxia)",
+"us-gov-east-1":"GovCloud (US-East)",
+"us-gov-west-1":"GovCloud (US-West)",
+"TBD":"Asia Pacific (Jakarta)",
+"me-south-1":"Middle East (Bahrain)",
+"af-south-1	":"Africa (Cape Town)",
+"ap-east-1":"Asia Pacific (Hong Kong)",
+"eu-south-1":"Europe (Milan)"
+}
+
+os = {
+"Linux/UNIX":"Linux",
+"Red Hat Enterprise Linux":"RHEL",
+"Red Hat Enterprise Linux with SQL Enterprise":"RHEL with SQL Ent",
+"Red Hat Enterprise Linux with SQL Standard":"RHEL with SQL Std",
+"Red Hat Enterprise Linux with SQL Web":"RHEL with SQL Web",
+"Red Hat Enterprise Linux with HA":"Red Hat Enterprise Linux with HA",
+"Red Hat Enterprise Linux with HA and SQL Enterprise":"Red Hat Enterprise Linux with HA with SQL Ent",
+"Red Hat Enterprise Linux with HA and SQL Standard":"Red Hat Enterprise Linux with HA with SQL Std",
+"SUSE":"SUSE",
+"Windows":"Windows",
+"Windows with SQL Enterprise":"Windows with SQL Ent",
+"Windows with SQL Standard":"Windows with SQL Std",
+"Windows with SQL Web":"Windows with SQL Web",
+"Linux with SQL Enterprise":"Linux with SQL Ent",
+"Linux with SQL Standard":"Linux with SQL Std",
+"Linux with SQL Web":"Linux with SQL Web",
+"BYOL":"BYOL"
+}
+
+commit = {
+"A":"All Upfront",
+"P":"Partial Upfront",
+"N":"No Upfront"
+}
+
+
